8a664f26755b3673806be4048dc543169c60d728
juanalee <optihyuny07@gmail.com>
2024-08-05 21:36:27 +0900

    updateadminheader

--- src/admin/components/adminHeader/AdminHeader.jsx
+++ src/admin/components/adminHeader/AdminHeader.jsx
@@ -42,25 +42,23 @@ const AdminHeader = () => {
 
   return (
     <div className={styles.adminHeaderContainer}>
-      <div className={styles.adminHeader}>
-        <div className={styles.adminHeaderTop}>
-          <div className={styles.adminHeaderLeftSection}>
-            <div className={styles.adminHeaderLogo}>
-              <img src="/img/lm_symbol_only.png" alt="symbol" />
-            </div>
-            <span className={styles.adminHeaderTitle}>관리자</span>
+      <div className={styles.adminHeaderTop}>
+        <div className={styles.adminHeaderLeftSection}>
+          <div className={styles.adminHeaderLogo}>
+            <img src="/img/lm_symbol_only.png" alt="symbol" />
           </div>
-          <div className={styles.adminHeaderRightSection}>
-            <span className={styles.adminHeaderInfo}>{memberNick}님</span>
-            <NavLink to="/" className={styles.adminHeaderLogoutLink} onClick={handleLogout}>로그아웃</NavLink>
-          </div>
+          <span className={styles.adminHeaderTitle}>관리자</span>
         </div>
-        <div className={styles.adminHeaderBottom}>
-          <NavLink to="/admin/members" className={styles.adminHeaderLink}>회원관리</NavLink>
-          <NavLink to="/admin/reports" className={styles.adminHeaderLink}>신고관리</NavLink>
-          <NavLink to="/admin/products" className={styles.adminHeaderLink}>상품관리</NavLink>
-          <NavLink to="/admin/categories" className={styles.adminHeaderLink}>카테고리 관리</NavLink>
+        <div className={styles.adminHeaderRightSection}>
+          <span className={styles.adminHeaderInfo}>{memberNick}님</span>
+          <NavLink to="/" className={styles.adminHeaderLogoutLink} onClick={handleLogout}>로그아웃</NavLink>
         </div>
+      </div>
+      <div className={styles.adminHeaderBottom}>
+        <NavLink to="/admin/members" className={styles.adminHeaderLink}>회원관리</NavLink>
+        <NavLink to="/admin/reports" className={styles.adminHeaderLink}>신고관리</NavLink>
+        <NavLink to="/admin/products" className={styles.adminHeaderLink}>상품관리</NavLink>
+        <NavLink to="/admin/categories" className={styles.adminHeaderLink}>카테고리 관리</NavLink>
       </div>
     </div>
   );
